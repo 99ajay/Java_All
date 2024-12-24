3341c250d926d1d8b6297f89d934503376ed3da5
Ajay Verma <tigersis1995@gmail.com>
2024-12-24 22:35:17 +0530

    Save image to the database and the make a student_address and done the basic operations

--- ProjectWithMaven/src/main/java/com/tut/App.java
+++ ProjectWithMaven/src/main/java/com/tut/App.java
@@ -1,5 +1,9 @@
 package com.tut;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -8,7 +12,7 @@ import org.hibernate.cfg.Configuration;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
         
         Configuration cfg = new Configuration();
@@ -19,6 +23,23 @@ public class App {
         st.setName("Ajay verma");
         st.setCity("Varanasi");
         
+        //creating oblect of the class Address_class
+        
+        Address ad = new Address();
+        ad.setStreet("street1");
+        ad.setCity("Lucknow");
+        ad.setOpen(true);
+        ad.setAddedDate(new Date());
+        ad.setX(1234.444);
+        //reading the images from the folder 
+        FileInputStream fis = new FileInputStream("src/main/java/image4.jpeg");
+        byte[] data = new byte[fis.available()];
+        fis.read(data);
+        ad.setImage(data);
+        
+        //reading the image 
+        
+        
         //factory will give us session
 //        Session session = factory.getCurrentSession();
         Session session = factory.openSession();
@@ -26,12 +47,21 @@ public class App {
         
         session.beginTransaction();
         session.save(st);
+        session.save(ad);
         session.getTransaction().commit();
         session.close();
+        
+        System.out.println("Doe BHai log");
         
         
 //        System.out.println(st);
 //        System.out.println(factory);
 //        System.out.println(factory.isClosed());
+        
+        
+        
+        
+        
+        
     }
 }
