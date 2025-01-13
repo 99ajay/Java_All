367e8cf3f05aa8ef9d26c36173636b57b0ba20f6
Ajay Verma <tigersis1995@gmail.com>
2025-01-13 16:39:54 +0530

    13/08/2024

--- webappSpringboot/src/main/java/com/webapp/controller/RegistrationController.java
+++ webappSpringboot/src/main/java/com/webapp/controller/RegistrationController.java
@@ -1,10 +1,13 @@
 package com.webapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.webapp.entity.Registration;
+import com.webapp.service.RegistrationService;
 
 @Controller
 public class RegistrationController {
@@ -16,16 +19,45 @@ public class RegistrationController {
 //	}
 //	
 	//http://localhost:8080/view
+	
+	@Autowired
+	private RegistrationService registrationService;
 	@RequestMapping("/view")
 	public String viewCreateRegistrationPage() {
 		return "create_registration";
 	}
 	
+//	@RequestMapping("/saveReg")
+//	public String getRegistrations(@ModelAttribute Registration registration) {
+//		System.out.println(registration.getFirstName());
+//		System.out.println(registration.getEmail());
+//		System.out.println(registration.getMobile());
+//
+//		return "create_registration";
+//	}
+	
+//	@RequestMapping("/saveReg")
+//	public String getRegistrations(@ModelAttribute Registration registration) {
+//		registrationService.createRegistration(registration);
+//		
+//
+//		return "create_registration";
+//	}
+	
+	
 	@RequestMapping("/saveReg")
-	public String getRegistrations(@ModelAttribute Registration registration) {
-		System.out.println(registration.getFirstName());
-		System.out.println(registration.getEmail());
-		System.out.println(registration.getMobile());
+	public String getRegistrations(
+			@RequestParam("firstName") String firstName,
+			@RequestParam("email") String email,
+			@RequestParam("mobile") String mobile
+			) {
+		Registration registration = new Registration();
+		registration.setFirstName(firstName);
+		registration.setEmail(email);
+		registration.setMobile(mobile);
+		
+		registrationService.createRegistration(registration);
+		
 
 		return "create_registration";
 	}
